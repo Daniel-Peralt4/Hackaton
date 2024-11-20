c941a81f9c78b9eabf654b1597ee93800b7b0580
JJCO10 <juanjosecarmonaortiz@gmail.com>
2024-11-20 15:49:14 -0500

    actualizacion chatbot

--- hackaton/src/Components/ChatBot.jsx
+++ hackaton/src/Components/ChatBot.jsx
@@ -1,41 +1,9 @@
 (function () {
-  window.__ow = window.__ow || {};
-  window.__ow.organizationId = "12b7f00e-bde9-477c-b236-63c17bd659f9";
-  window.__ow.template_id = "89c2de23-5771-4aa9-8b4e-867322f17c92";
-  window.__ow.integration_name = "manual_settings";
-  window.__ow.product_name = "chatbot";   
-  (function(n, t, c) {
-      function i(n) {
-          return e._h ? e._h.apply(null, n) : e._q.push(n);
-      }
-      var e = {
-          _q: [],
-          _h: null,
-          _v: "2.0",
-          on: function () {
-              i(["on", c.call(arguments)]);
-          },
-          once: function () {
-              i(["once", c.call(arguments)]);
-          },
-          off: function () {
-              i(["off", c.call(arguments)]);
-          },
-          get: function () {
-              if (!e._h) throw new Error("[OpenWidget] You can't use getters before load.");
-              return i(["get", c.call(arguments)]);
-          },
-          call: function () {
-              i(["call", c.call(arguments)]);
-          },
-          init: function () {
-              var n = t.createElement("script");
-              n.async = !0;
-              n.type = "text/javascript";
-              n.src = "https://cdn.openwidget.com/openwidget.js";
-              t.head.appendChild(n);
-          }
-      };
-      !n.__ow.asyncInit && e.init(), n.OpenWidget = n.OpenWidget || e;
-  })(window, document, [].slice);
-})();
+    window.__ow = window.__ow || {};
+    window.__ow.organizationId = "12b7f00e-bde9-477c-b236-63c17bd659f9";
+    window.__ow.template_id = "78ae9565-2217-441a-974e-0f3a8ddf9279";
+    window.__ow.integration_name = "manual_settings";
+    window.__ow.product_name = "chatbot";   
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};!n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e}(window,document,[].slice))
+  
+  })();
